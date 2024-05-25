2c1acaf4f73a0d62e373196f500b61f3b765a440
maria <94370096+mariarodriguez217@users.noreply.github.com>
2024-05-25 20:31:06 -0300

    nueva

---
+++ 03-Dominio/Entidades/Doctor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using _03_Dominio.ValueObjects;
+
+namespace _03_Dominio.Entidades
+{
+    public class Doctor
+    {
+        IdD idd;
+        Nombre nombre;
+        Apellido apellido;
+        Especialidad especialidad;
+        FechaIngreso fechaingreso;
+        
+        public Doctor (Guid idd, string nombre,string apellido,Especilidad especialidad,DateTime fechaingreso )
+        {
+            this.idd = new Id(id);
+            this.nombre = new Nombre(nombre);
+            this.apellido = new Apellido(apellido);
+            this.especialidad = especialidad;
+            this.fechaingreso = new FechaIngreso(fechaingreso);
+        }
+        public Guid idd()
+        {
+            return this.idd.Valor();
+        }
+        public string Nombre()
+        {
+            return this.nombre.Valor();
+        }
+        public string Apellido()
+        {
+            return this.apellido.Valor();
+        }
+        public string Especialidad()
+        {
+            return this.especialidad.Valor();
+        }
+        public DateTime FechaIngreso()
+        {
+            return this.fechaingreso.Valor();
+        }
+
+
+
+
+
+
+    }
+}
---
+++ 03-Dominio/Entidades/Paciente.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using _03_Dominio.ValueObjects;
+namespace _03_Dominio.Entidades
+{
+    public class Paciente
+    {
+        IdP idp;
+        NombreP nombrep;
+        ApellidoP apellidop;
+        FechaNacimiento fechanacimiento;
+
+        public Paciente(Guid idp, string nombrep, string apellidop, DateTime fechanacimiento)
+        {
+            this.idp = new IdP(idp);
+            this.nombrep = new NombreP(nombrep);
+            this.apellidop = ApellidoP(apellidop);
+            this.fechanacimiento = new FechaNacimiento(fechanacimiento);
+
+        }
+        public Guid IdP()
+        {
+            return this.idp.Valor();
+        }
+        public string NombreP()
+        {
+            return this.nombrep.Valor();
+        }
+        public string ApellidoP ()
+        {
+            return this.apellidop.Valor();
+        }
+
+        public DateTime FechaNacimiento()
+        {
+            return this.fechanacimiento.Valor();
+        }
+
+    }
+}
+
+}
